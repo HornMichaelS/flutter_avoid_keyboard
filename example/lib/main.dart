b9521f22165f43294d84a524ce5577559479f58e
Michael Horn <hornmichaels@gmail.com>
2021-06-05 08:20:44 -0700

    Update pubspec.yaml to meet pub.dev standards

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(title: Text("Avoid Keyboard Example")),
         body: AvoidKeyboardExample(),
