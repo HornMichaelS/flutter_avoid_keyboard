5e5a6439a42b42a1f6441029d7c0b2a5760e0754
Michael Horn <hornmichaels@gmail.com>
2021-06-10 12:51:11 -0700

    Update example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,9 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Avoid Keyboard Example',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(title: Text("Avoid Keyboard Example")),
@@ -31,24 +29,65 @@ class AvoidKeyboardExample extends StatelessWidget {
       spacing: 40,
       child: Container(
         padding: EdgeInsets.all(30),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            TextField(textInputAction: TextInputAction.next),
-            TextField(
-              textInputAction: TextInputAction.next,
-              autocorrect: false,
-            ),
-            TextField(
-              textInputAction: TextInputAction.next,
-              autocorrect: false,
-            ),
-            TextField(
-              textInputAction: TextInputAction.next,
-              autocorrect: false,
-            ),
-            TextField(),
-          ],
+        child: FocusTraversalGroup(
+          policy: OrderedTraversalPolicy(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              exampleField(
+                color: Colors.blue,
+                action: TextInputAction.next,
+                order: 0,
+              ),
+              exampleField(
+                color: Colors.purple[200],
+                action: TextInputAction.next,
+                order: 1,
+              ),
+              exampleField(
+                color: Colors.red[200],
+                action: TextInputAction.next,
+                order: 2,
+              ),
+              exampleField(
+                color: Colors.orange[200],
+                action: TextInputAction.next,
+                order: 3,
+              ),
+              exampleField(
+                color: Colors.yellow[100],
+                order: 4,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget exampleField({
+    TextInputAction? action,
+    Color? color,
+    bool? autocorrect,
+    double? order,
+  }) {
+    return Container(
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: FocusTraversalOrder(
+        order: NumericFocusOrder(order ?? 0),
+        child: TextField(
+          cursorColor: Colors.grey,
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.only(left: 10),
+            focusedBorder: InputBorder.none,
+            disabledBorder: InputBorder.none,
+            border: InputBorder.none,
+          ),
+          autocorrect: autocorrect ?? true,
+          textInputAction: action,
         ),
       ),
     );
